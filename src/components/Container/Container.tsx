1a4b92dbd9dffa858d867ebeba6292e75a209f91
Sofia <dyadechko.sofiya@gmail.com>
2025-09-11 17:49:35 +0500

    add Container

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,14 @@
+import type { FC, HTMLProps, PropsWithChildren } from "react"
+import { cn } from "../../utils";
+
+interface Props extends PropsWithChildren<HTMLProps<HTMLDivElement>> {
+	className?: string;
+}
+
+export const Container: FC<Props> = ({children, className, ...props}) => {
+	return (
+		<div className={cn("container", "mx-auto", "max-w-[1440px]", "w-full" ,"px-20")} {...props}>
+			{children}
+		</div>
+	)
+}
