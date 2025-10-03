eb29e65b1ca45a7df4db5b77bdb207b5e16abcb7
Sofia <dyadechko.sofiya@gmail.com>
2025-10-03 12:53:35 +0500

    add MainPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router';
 import './App.css';
 import { Layout } from './components';
+import { MainPage } from './pages';
 
 function App() {
 	return (
@@ -8,8 +9,8 @@ function App() {
 			<Route
 				path="/"
 				element={<Layout />}>
-					{/* <Route index element={<MainPage/>}/>
-					<Route path='genres' element={<GenresPage/>}/>
+					<Route index element={<MainPage/>}/>
+					{/*<Route path='genres' element={<GenresPage/>}/>
 					<Route path='genres/:genreName' element={<GenrePage/>}/>
 					<Route path='about-movie/:movieId' element={<AboutMovie/>}/>
 					<Route path='profile' element={<ProfileLayout />}>
